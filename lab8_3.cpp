335da765493a540b4a12fd92191052dfa3c916ef
Supanat <k.suphanuch23@gmail.com>
2024-12-09 22:07:35 +0700

    Lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,46 @@
 "Brook"
 "Jinbe"
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int long long age,H,B;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> H;
+        if (H < 100){
+            cout << "Your character = Chopper";
+        }
+        else if (H >= 100 && H <180){
+            cout << "Your character = Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> B;
+            if (B > 1100000000){
+                cout << "Your character = Zoro";
+            }
+            else{
+                cout << "Your character = Sanji";
+            }
+        }
+    }
+    else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> B;
+        if (B>500000000){
+            cout << "Your character = Jinbe";
+        }
+        else{
+            cout << "Your character = Franky";
+        }
+    }
+    else if (age > 60){
+        cout << "Your character = Brook";
+    }
+}
